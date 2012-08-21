9acb184b3f1a2df5e12f3612f4a173d1884d3dae
aman <aman.mathur.1993@gmail.com>
2012-08-21 11:37:46 +0530

    This should include Parser and Example1..

---
+++ Test/src/bookviz/Example1.java
@@ -0,0 +1,87 @@
+package bookviz;
+
+import java.awt.Color;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+import prefuse.Constants;
+import prefuse.Display;
+import prefuse.Visualization;
+import prefuse.action.ActionList;
+import prefuse.action.RepaintAction;
+import prefuse.action.assignment.ColorAction;
+import prefuse.action.assignment.DataColorAction;
+import prefuse.action.layout.RandomLayout;
+import prefuse.controls.DragControl;
+import prefuse.controls.PanControl;
+import prefuse.controls.ZoomControl;
+import prefuse.data.Graph;
+import prefuse.render.DefaultRendererFactory;
+import prefuse.render.ShapeRenderer;
+import prefuse.util.ColorLib;
+import prefuse.visual.VisualItem;
+
+public class Example1 {
+	private static Graph graph;
+	private static Visualization vis;
+	private static Display d;
+	public static void main(String [] argv) throws IOException{
+		Parser a = new Parser();
+		graph = a.parsebook();
+		System.out.println(graph.getNodeCount());
+		setUpVisualization();
+		setUPRenderers();
+		setUpActions();
+		setUpDisplay();
+		JFrame frame = new JFrame("graph");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(d);
+		frame.pack();
+		frame.setVisible(true);
+		vis.run("color");
+		vis.run("layout");
+		}
+	
+	
+	private static void setUpDisplay() {
+		d = new Display(vis);
+		d.setSize(720, 500);
+		d.addControlListener(new DragControl());
+		d.addControlListener(new PanControl());
+		d.addControlListener(new ZoomControl());
+		d.setBackground(Color.BLACK);
+	}
+	
+	
+
+
+	private static void setUpActions() {
+		int[] palette = {ColorLib.rgb(200, 0, 0), ColorLib.rgb(200,200, 200), ColorLib.rgb(0,  200, 0)}; 
+		DataColorAction fill = new DataColorAction("pol.nodes","value", Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
+		ColorAction edges = new ColorAction("pol.edges",VisualItem.STROKECOLOR,ColorLib.rgb(0, 0, 200));
+		ActionList color = new ActionList();
+		color.add(fill);
+		color.add(edges);
+		ActionList layout = new ActionList();		
+		layout.add(new RandomLayout("pol"));
+		layout.add(new RepaintAction());
+		vis.putAction("color", color);
+		vis.putAction("layout", layout);
+	}
+
+
+	private static void setUPRenderers() {
+		ShapeRenderer r = new ShapeRenderer();
+		vis.setRendererFactory(new DefaultRendererFactory(r));
+	}
+	
+	
+	private static void setUpVisualization() {
+		vis = new Visualization();
+		vis.add("pol", graph);
+	}
+}
+
+
+
---
+++ Test/src/bookviz/Parser.java
@@ -0,0 +1,157 @@
+package bookviz;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import prefuse.data.Graph;
+import prefuse.data.Node;
+
+public class Parser {
+	Graph pol;
+	Graph blog;
+	public Graph parsebook() throws IOException{
+		System.out.println("Enter the location of the file:");
+		Scanner file = new Scanner(System.in);
+		String file1 = file.nextLine();
+		FileReader read = new FileReader(file1);
+		BufferedReader reader = new BufferedReader(read);
+		@SuppressWarnings("unused")
+		String creator = reader.readLine();
+		@SuppressWarnings("unused")
+		String graph = reader.readLine();
+		@SuppressWarnings("unused")
+		String waste = reader.readLine();
+		String directed = reader.readLine();
+		StringTokenizer direction = new StringTokenizer(directed, "d ");
+		direction.nextToken();
+        int bool = Integer.parseInt(direction.nextToken());
+        if (bool == 1){
+        	pol = new Graph(true);
+        	pol.addColumn("id", int.class);
+        	pol.addColumn("label", String.class);
+        	pol.addColumn("value", String.class);
+        }
+        else {
+        	pol = new Graph(false);
+        	pol.addColumn("id", int.class);
+        	pol.addColumn("label", String.class);
+        	pol.addColumn("value", String.class);
+        }
+		while(true){
+			String line = reader.readLine();
+			if (line==null){
+				break;
+			}
+			else if (line.equals("  node")){
+				@SuppressWarnings("unused")
+				String waste1 = reader.readLine();
+				Node book = pol.addNode();
+				String id = reader.readLine();
+				StringTokenizer id1 = new StringTokenizer(id, "d ");
+				id1.nextToken();
+		        int no = Integer.parseInt(id1.nextToken());
+		        book.set("id",no);
+		        String label = reader.readLine();
+				StringTokenizer label1 = new StringTokenizer(label, "l ");
+				label1.nextToken();
+				book.set("label", label1.nextToken());
+				String value = reader.readLine();
+				StringTokenizer value1 = new StringTokenizer(value, "e ");
+				value1.nextToken();
+				book.set("value",value1.nextToken());
+			}
+			else if (line.equals("  edge")){
+				@SuppressWarnings("unused")
+				String waste2 = reader.readLine();
+				String source = reader.readLine();
+				StringTokenizer source1 = new StringTokenizer(source, "e ");
+				source1.nextToken();
+		        int src = Integer.parseInt(source1.nextToken());
+		        String target = reader.readLine();
+				StringTokenizer target1 = new StringTokenizer(target, "t ");
+				target1.nextToken();
+		        int tar = Integer.parseInt(target1.nextToken());
+		        pol.addEdge(src, tar);
+			}
+			else {
+				continue;
+			}
+		}
+		
+		return pol;
+	}
+	
+	public void parseblog() throws IOException{
+		System.out.println("Enter the location of the file:");
+		Scanner file = new Scanner(System.in);
+		String file1 = file.nextLine();
+		FileReader read = new FileReader(file1);
+		BufferedReader reader = new BufferedReader(read);
+		@SuppressWarnings("unused")
+		String creator = reader.readLine();
+		@SuppressWarnings("unused")
+		String graph = reader.readLine();
+		String directed = reader.readLine();
+		StringTokenizer direction = new StringTokenizer(directed, "d ");
+		direction.nextToken();
+        int bool = Integer.parseInt(direction.nextToken());
+        if (bool == 1){
+        	Graph blog = new Graph(true);
+        	blog.addColumn("id", int.class);
+        	blog.addColumn("label", String.class);
+        	blog.addColumn("value", int.class);
+        	blog.addColumn("source", String.class);
+        }
+        else {
+        	Graph blog = new Graph(false);
+        	blog.addColumn("id", int.class);
+        	blog.addColumn("label", String.class);
+        	blog.addColumn("value", int.class);
+        	blog.addColumn("source", String.class);
+        }
+		while(true){
+			String line = reader.readLine();
+			if (line==null){
+				break;
+			}
+			else if (line.equals("  node [")){
+				Node blogs = pol.addNode();
+				String id = reader.readLine();
+				StringTokenizer id1 = new StringTokenizer(id, "d ");
+				id1.nextToken();
+		        int no = Integer.parseInt(id1.nextToken());
+		        blogs.set("id",no);
+		        String label = reader.readLine();
+				StringTokenizer label1 = new StringTokenizer(label, "l ");
+				label1.nextToken();
+				blogs.set("label", label1.nextToken());
+				String value = reader.readLine();
+				StringTokenizer value1 = new StringTokenizer(value, "e ");
+				id1.nextToken();
+				int no1 = Integer.parseInt(value1.nextToken());
+				blogs.set("value",no1);
+				String source = reader.readLine();
+				StringTokenizer source1 = new StringTokenizer(source, "e ");
+				source1.nextToken();
+				blogs.set("source",source1.nextToken());
+			}
+			else if (line.equals("  edge [")){
+				String source = reader.readLine();
+				StringTokenizer source1 = new StringTokenizer(source, "e ");
+				source1.nextToken();
+		        int src = Integer.parseInt(source1.nextToken());
+		        String target = reader.readLine();
+				StringTokenizer target1 = new StringTokenizer(target, "t ");
+				target1.nextToken();
+		        int tar = Integer.parseInt(target1.nextToken());
+		        blog.addEdge(src, tar);
+			}
+			else {
+				continue;
+			}
+		}
+	}
+}
